1e3d648829fd750bf2ef8b7a93d16bafdd9d65b1
Leawind <leawind@yeah.net>
2025-10-29 20:54:26 +0800

    export TuzkManager

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { CompositeTuzk, Tuzk } from '@/tuzk.ts';
+export { TuzkManager } from '@/manager.ts';
 export { type ActiveTuzk, type TuzkLike, type TuzkRunner, TuzkState } from '@/types.ts';
 export * as errors from '@/errors.ts';
